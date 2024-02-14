483a197e50680e93c368df47ad4a5bbc834d0a2d
Sean Eric Thomas <set@seanericthomas.com>
2024-02-13 19:51:28 -0800

    Add new user to database

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -1,11 +1,103 @@
+import bcrypt from "bcrypt";
 import asyncHandler from "express-async-handler";
+import { body, matchedData, validationResult } from "express-validator";
 
-const createNewUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: "thanks!",
-    body: req.body,
-  });
-});
+import UserInterface from "@interfaces/Users";
+import UserModel from "@models/user";
+
+const createNewUser = [
+  body("email")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Email required")
+    .isLength({ min: 3, max: 255 })
+    .withMessage("Email must be between 3-255 characters")
+    .isEmail()
+    .withMessage("Invalid email")
+    .custom(async (value) => {
+      const existingUser = await UserModel.findOne({ email: value });
+      if (existingUser) {
+        throw new Error("Email already in use");
+      }
+    }),
+
+  body("username")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Username required")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username must be between 3-20 characters")
+    .custom(async (value) => {
+      const existingUser = await UserModel.findOne({ username: value });
+      if (existingUser) {
+        throw new Error("Username already taken");
+      }
+    }),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage("Password does not meet requirements"),
+
+  body("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name required")
+    .isLength({ max: 255 })
+    .withMessage("Name cannot be more than 255 characters"),
+
+  asyncHandler(async (req, res) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      res.status(400);
+      res.json({
+        message: "Invalid form data - see 'errors' for detail",
+        errors: validationErrors.mapped(),
+      });
+    }
+
+    const data = matchedData(req);
+
+    try {
+      const hashedPass = await bcrypt.hash(data.password, 10);
+
+      const userInfo: UserInterface = {
+        email: data.email,
+        followers: [],
+        following: [],
+        lastLogin: new Date(),
+        name: data.name,
+        password: hashedPass,
+        username: data.username,
+      };
+
+      const newUser = new UserModel(userInfo);
+      const userDoc = await newUser.save();
+
+      res.status(201);
+      res.json({
+        message: "User created",
+        user: userDoc,
+      });
+    } catch(err) {
+      res.status(500);
+      res.json({
+        message: "Error when creating new user",
+        error: err,
+      });
+    }
+  }),
+];
 
 const usersController = {
   createNewUser,
--- interfaces/Users.ts
+++ interfaces/Users.ts
@@ -1,10 +1,10 @@
 export default interface UserInterface {
-  email: string;
-  password: string;
-  name: string;
-  username: string;
+  avatar?: string;
+  email: string; // unique, 255 chars max
   followers: string[];
   following: string[];
-  avatar?: string;
   lastLogin: Date;
+  name: string; // 255 chars max
+  password: string; // 8 chars min, 1 upper, 1 lower, 1 number, 1 symbol
+  username: string; // unique, between 3-20 characters
 }
