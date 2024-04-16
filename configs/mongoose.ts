8154be989cb35b15c6f776520c3170b717a651d8
Sean Eric Thomas <set@seanericthomas.com>
2024-04-15 20:07:13 -0700

    Add options to mongo connection

--- configs/mongoose.ts
+++ configs/mongoose.ts
@@ -3,14 +3,24 @@ import mongoose from "mongoose";
 
 // setup environemnt variables
 dotenvConfig();
+const NODE_ENV = process.env.NODE_ENV;
 const MONGO = process.env.MONGO;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
+
+const options: {user?: string, pass?: string} = {};
+
+if (NODE_ENV === "production" && MONGO_USER && MONGO_PASS) {
+  options.user = MONGO_USER;
+  options.pass = MONGO_PASS;
+}
 
 // setup mongoose
 mongoose.set("strictQuery", true);
 export default async function connectMongoDB() {
   try {
     if (MONGO) {
-      await mongoose.connect(MONGO);
+      await mongoose.connect(MONGO, options);
       console.log("Connected to MongoDB");
     } else {
       throw new Error(
